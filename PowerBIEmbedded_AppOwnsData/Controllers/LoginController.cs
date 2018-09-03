e90072f3759f0be453e5ab93c87d4a7601db7789
voicenotegitlab@gmail.com <voicenotegitlab2018>
2018-09-03 20:14:35 +0530

    first

---
+++ PowerBIEmbedded_AppOwnsData/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PowerBIEmbedded_AppOwnsData.Models;
+
+namespace PowerBIEmbedded_AppOwnsData.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(PowerBIEmbedded_AppOwnsData.Models.CustomerMaster customerModel)
+        {
+            using (TestDBTestDBEntities db = new TestDBTestDBEntities())
+            {
+                var customerDetails = db.CustomerMasters.Where(x => x.Email == customerModel.Email && x.Password == customerModel.Password).FirstOrDefault();
+                if (customerDetails == null)
+                {
+                    customerModel.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", customerModel);
+                }
+                else
+                {
+                    Session["UserName"] = customerDetails.UserName;
+                    Session["Email"] = customerDetails.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
